1f765f07036862622ef86e9d5a8d893984f3dabb
thomas-osgood <20747585+thomas-osgood@users.noreply.github.com>
2024-07-09 18:55:06 -0400

    fix(genid): UUID Library Used In SSEChannelMap (#6)
    Co-authored-by: thomas-osgood <thomas-osgood@users.noreply.github.com>

--- types.go
+++ types.go
@@ -1,6 +1,8 @@
 package ssehandler
 
+import "github.com/google/uuid"
+
 type HeaderMap map[string]string
 type SSEChannel chan SSEMessage
-type SSEChannelMap map[string]SSEChannel
+type SSEChannelMap map[uuid.UUID]SSEChannel
 type SSEHandlerOptFunc func(*SSEHandlerOption) error
