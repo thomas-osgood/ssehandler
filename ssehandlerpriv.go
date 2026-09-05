1f4284f73f310c1f5ac80de4bf82179f8b378174
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-04 21:15:21 -0400

    removed unnecssary recursion and check in generateID because of such small chance of collision

--- ssehandlerpriv.go
+++ ssehandlerpriv.go
@@ -1,12 +1,10 @@
 package ssehandler
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/google/uuid"
 	sseconst "github.com/thomas-osgood/ssehandler/internal/constants"
-	ssemsg "github.com/thomas-osgood/ssehandler/internal/messages"
 )
 
 // function designed to execute the logic for when a client
@@ -28,29 +26,11 @@ func (sh *SSEHandler) clientIdExists(id uuid.UUID) (exists bool) {
 }
 
 // function designed to generate a unique id for a client using a-zA-Z0-9.
-func (sh *SSEHandler) generateID(attempt int) (id uuid.UUID, err error) {
+func (sh *SSEHandler) generateID() (id uuid.UUID, err error) {
 
 	id, err = uuid.NewUUID()
 	if err != nil {
 		return uuid.Nil, err
-	}
-
-	// if the id already exists in the map, attempt to
-	// generate another id.
-	//
-	// if the maximum number of attempts has been reached
-	// return an error.
-	//
-	// if the maximum number of attempts has not yet been
-	// reached, the attempt number will be incremented and
-	// this function will be called recursively.
-	if sh.clientIdExists(id) {
-		if attempt >= sseconst.GENERATE_ATTEMPT_MAX {
-			return uuid.Nil, fmt.Errorf(ssemsg.ERR_GENERATEID_MAXATTEMPTS)
-		}
-
-		attempt++
-		return sh.generateID(attempt)
 	}
 
 	return id, nil
