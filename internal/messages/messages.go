dc536260bc6015fc156f1ed46a67bb34019e6840
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-13 18:05:47 -0400

    added ERR_EMPTY_ORIGIN

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -1,6 +1,7 @@
 package messages
 
 const ERR_EMPTY_MAP string = "client map cannot be nil"
+const ERR_EMPTY_ORIGIN string = "origin must be a non-zero length string"
 const ERR_GENERATEID_MAXATTEMPTS string = "unable to generate a client id in max number of attempts"
 const ERR_MAXMIN_LEN string = "min and max lengths must be greater than zero"
 const ERR_MIN_LEN string = "min length must be less than or equal to max length"
