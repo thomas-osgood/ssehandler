3124332c8d266e4df31ea48b876a422c05f6e6a8
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-20 18:14:33 -0400

    added ERR_MAXAGE_NOTPOS

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -3,6 +3,7 @@ package messages
 const ERR_EMPTY_MAP string = "client map cannot be nil"
 const ERR_EMPTY_ORIGIN string = "origin must be a non-zero length string"
 const ERR_GENERATEID_MAXATTEMPTS string = "unable to generate a client id in max number of attempts"
+const ERR_MAXAGE_NOTPOS string = "max age must be a positive value"
 const ERR_MAXMIN_LEN string = "min and max lengths must be greater than zero"
 const ERR_MIN_LEN string = "min length must be less than or equal to max length"
 const ERR_RANDSTR_LEN string = "error generating the length: %s"
