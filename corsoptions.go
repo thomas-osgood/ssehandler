83b00f973a7c11d2536b0fa7bc4d332005c5a1ad
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-22 19:15:20 -0400

    began cleanMethods func

--- corsoptions.go
+++ corsoptions.go
@@ -5,6 +5,18 @@ import (
 )
 
 /*
+normalize each element, remove empty strings, remove duplicates
+and remove invalid methods in the AllowedMethods slice.
+*/
+func (co *CorsOptions) cleanMethods() {
+	if co == nil || len(co.AllowedMethods) < 1 {
+		return
+	}
+
+	co.AllowedMethods = utils.UniqueSlice(co.AllowedMethods)
+}
+
+/*
 normalize (trim + lower) each element in the Origins slice
 and remove the empty strings.
 */
