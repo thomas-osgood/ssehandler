03587cd0e5f2c81a8259efaee71584f847108d67
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-23 22:09:30 -0400

    updated messages

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -1,7 +1,8 @@
 package messages
 
 const ERR_EMPTY_MAP string = "client map cannot be nil"
-const ERR_EMPTY_HEADER string = "expose header must be a non-zero length string"
+const ERR_EMPTY_HEADER_ALLOW string = "allow header must be a non-zero length string"
+const ERR_EMPTY_HEADER_EXPOSE string = "expose header must be a non-zero length string"
 const ERR_EMPTY_METHOD string = "method must be a non-zero length string"
 const ERR_EMPTY_ORIGIN string = "origin must be a non-zero length string"
 const ERR_GENERATEID_MAXATTEMPTS string = "unable to generate a client id in max number of attempts"
