e9e903bb296631a86c9631a4acbfdb1b06f8dee5
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-09-24 19:47:07 -0400

    added more headers to setHeaders

--- ssehandlerpriv.go
+++ ssehandlerpriv.go
@@ -2,6 +2,7 @@ package ssehandler
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/google/uuid"
@@ -41,13 +42,24 @@ func (sh *SSEHandler) setHeaders(w http.ResponseWriter) {
 	var headerName string
 	var headerValue string
 	var corsOrigins string = strings.Join(sh.corssettings.Origins, " ")
+	var exposeHeaders string = strings.Join(sh.corssettings.ExposeHeaders, ", ")
 
 	// set the headers necessary for the server-sent-events to work.
 	w.Header().Set(sseconst.HEADER_ACALLOW_NAM, corsOrigins)
-	w.Header().Set(sseconst.HEADER_ACEXPOSE_NAM, sseconst.HEADER_ACEXPOSE_VAL)
+	w.Header().Set(sseconst.HEADER_ACEXPOSE_NAM, exposeHeaders)
 	w.Header().Set(sseconst.HEADER_ACCELBUFFER_NAM, sseconst.HEADER_ACCELBUFFER_VAL)
 	w.Header().Set(sseconst.HEADER_CONTENTTYPE_NAM, sseconst.HEADER_CONTENTTYPE_VAL)
 	w.Header().Set(sseconst.HEADER_CACHE_NAM, sseconst.HEADER_CACHE_VAL)
+
+	// set CORS headers if needed
+	w.Header().Set(sseconst.HEADER_ACCREDS_NAM, strconv.FormatBool(sh.corssettings.AllowCredentials))
+	if sh.corssettings.MaxAge > 0 {
+		w.Header().Set(sseconst.HEADER_ACMAXAGE_NAM, strconv.Itoa(sh.corssettings.MaxAge))
+	}
+
+	if len(sh.corssettings.AllowedMethods) > 0 {
+		w.Header().Set(sseconst.HEADER_ACMETHODS_NAM, strings.Join(sh.corssettings.AllowedMethods, ", "))
+	}
 
 	// set the user-defined custom headers.
 	for headerName, headerValue = range sh.customHeaders {
